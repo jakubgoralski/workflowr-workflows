881cc9a12587dac3640305bdc23316b105152fd0
jakubgoralski <49205231+jakubgoralski@users.noreply.github.com>
2023-04-26 14:57:50 +0200

    2: added missing file

---
+++ WorkflowR.Workflows.Domain/Tasking/Workflow.cs
@@ -0,0 +1,12 @@
+﻿using WorkflowR.Workflows.Domain.Abstraction;
+
+namespace WorkflowR.Workflows.Domain.Tasking
+{
+    internal class Workflow : IAggregate
+    {
+        public AggregateRoot<Task> aggregateRoot { get; private set; }
+
+        private TaskStatus WorkflowStatus = new TaskStatus(Status.None);
+        private List<Task> tasks = new List<Task>();
+    }
+}
